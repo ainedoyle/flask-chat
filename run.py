e90c3e2ed8b52bf871216ffd04b1d822cdd12f1f
Aine Doyle <doy89@outlook.ie>
2018-08-08 11:06:13 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return "<h1>Hello There!</h1>"
-
+    return "<h1>Hello There</h1>"
+    
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
